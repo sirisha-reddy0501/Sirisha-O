c2d17038297a797db2adaf1e60b0f055c948f342
Sirisha Reddy <119389139+sirisha-reddy0501@users.noreply.github.com>
2025-06-10 18:03:48 +0530

    Update socialsData.js

--- src/data/socialsData.js
+++ src/data/socialsData.js
@@ -1,5 +1,5 @@
 export const socialsData = {
-    github: 'https://github.com/22003264',
+    github: 'https://github.com/sirisha-reddy0501',
     linkedIn: 'https://www.linkedin.com/in/sirisha-onteddu-017618336/',
     leetcode: 'https://leetcode.com/u/sirishareddy121',
     gmail: 'mailto:reddysirisha0501@gmail.com'
